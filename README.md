0dd475b108d90b0cce50dcaa57bf5ae9867e9202
crx <957188082@qq.com>
2025-07-31 20:29:00 +0800

    test2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Doramon_MemoryBread
+# 🥖 Doramon 记忆面包
+## 📁 项目结构
 Doramon_MemoryBread/
 ├── main.py           # 主程序
 ├── ai_analyzer.py    # AI分析模块
@@ -8,7 +9,21 @@ Doramon_MemoryBread/
 └── sample/
     └── sample1       # 示例访谈文件
 
-python main.py 即可
-
 1. ai_analyzer.py对文本信息大模型处理输出
 2. feishu_writer.py将处理后的信息写入飞书文档
+
+马上增加：
+1. 飞书的设计
+2. 声音转文本api，输入为声音
+
+## 🚀 快速开始
+```bash
+python main.py 即可
+```
+## 🚀 git指令
+git上传指令：
+```bash
+1. git add .
+2. git commit -m "test"
+3. git push -u origin main
+```
--- ai_analyzer.py
+++ ai_analyzer.py
@@ -3,11 +3,11 @@ from config import MINIMAX_GROUP_ID, MINIMAX_API_KEY
 
 def analyze_interview(text):
     """调用MiniMax API分析访谈内容"""
-    url = f"https://api.minimaxi.com/v1/text/chatcompletion_pro?GroupId={MINIMAX_GROUP_ID}"
+    url = f"https://api.minimaxi.com/v1/text/chatcompletion_pro?GroupId={MINIMAX_GROUP_ID}" # api接口地址
     headers = {
         "Authorization": f"Bearer {MINIMAX_API_KEY}", 
         "Content-Type": "application/json"
-    }
+    } # http请求头：身份验证+json格式
     
     payload = {
         "model": "MiniMax-Text-01",
@@ -22,14 +22,17 @@ def analyze_interview(text):
                 "content": "Kairos是一个思想储存助手。kairos可以总结用户输入的文本信息，判断总结里面灵感生成的片刻，然后总结相应的灵感，并且生成围绕灵感需要进一步所做的执行操作。"
             }
         ]
-    }
+    } # 请求数据构建
+    # reply_constraints用于设置api回复的格式和身份，当前为机器人（大的提示词）
+    # messages提供对话的上下文信息，发送者类型、名称、消息等；当前统一为用户，无法区分不同用户：我认为未来可以有不同的场景如面试官和候选人，根据前期大模型对于信息处理猜测场景/用户手动切换  1.灵感 2.日程规划
+    # 定义AI助手的角色、性格和能力等（小的提示词）
     
     try:
-        response = requests.post(url, headers=headers, json=payload)
+        response = requests.post(url, headers=headers, json=payload) # 发送POST请求到API，json=payload ：将payload自动转换为JSON格式发送
         response.raise_for_status()  # 抛出HTTP错误
-        response_data = response.json()
+        response_data = response.json() #将响应内容转换为json格式
         
-        return response_data.get("reply", "AI未返回分析结果")
+        return response_data.get("reply", "AI未返回分析结果") #安全地获取字典中的"reply"键值
             
     except requests.exceptions.RequestException as e:
         return f"网络请求错误: {str(e)}"
